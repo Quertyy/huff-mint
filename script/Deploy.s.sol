7cb8f0df8873a89ad9528fd191cf3ae9c979be06
Quertyy <qwertyense@protonmail.com>
2023-04-04 20:05:03 +0200

    refactor: add generic view method

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,13 +4,24 @@ pragma solidity ^0.8.15;
 import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Script.sol";
 
-interface SimpleStore {
-  function setValue(uint256) external;
-  function getValue() external returns (uint256);
+interface SimpleMintHuff {
+    function owner() external view returns (address);
+    function withdraw() external;
+    
+    function publicMint(uint256) external payable;
+
+    function price() external view returns (uint256);
+    function maxMint() external view returns (uint256);
+    function maxSupply() external view returns (uint256);
+    function counter() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+    function ownerOf(uint256 tokenId) external view returns (address);
 }
 
 contract Deploy is Script {
-  function run() public returns (SimpleStore simpleStore) {
-    simpleStore = SimpleStore(HuffDeployer.deploy("SimpleStore"));
+  function run() public returns (SimpleMintHuff mintHuff) {
+      address impl = HuffDeployer.deploy("SimpleMintHuff");
+      mintHuff = SimpleMintHuff(impl);
   }
 }
